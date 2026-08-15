95f85250598d64189be6d8ef931b9f2445008eb5
Shantanu2705 <shantanubiswas951@gmail.com>
2026-08-15 16:23:30 +0530

    Remove scroll icon from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,11 +44,6 @@ export default function Home() {
             </Link>
           </div>
         </div>
-        <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce">
-          <div className="w-8 h-12 rounded-full border-2 border-white/50 flex items-start justify-center p-2">
-            <div className="w-1 h-3 bg-white rounded-full animate-pulse" />
-          </div>
-        </div>
       </section>
 
       {/* Trust Strip */}
